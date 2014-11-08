6f7efe1ef5b263e2ac5099e95369144beadc7ce5
Nadi <qodovo@gmail.com>
2014-11-07 21:44:36 -0800

    Move PanelController to a custom element directive

--- app.js
+++ app.js
@@ -6,16 +6,6 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function(){
-    this.tab = 1;
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    }
-  });
-
   app.controller('GalleryController', function(){
     this.current = 0;
     this.setCurrent = function(val){
@@ -29,6 +19,23 @@
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    }
+  });
+
+  app.directive('productPanels', function(){
+    return {
+      restrict      : 'E',
+      templateUrl   : "product-panels.html",
+      controller    : function(){
+        this.tab = 1;
+        this.selectTab = function(setTab){
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        }
+      },
+      controllerAs  : 'panel'
     }
   });
 
