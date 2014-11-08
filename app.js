15b0b448e161a4522203dd43beceac29171a4042
Nadi <qodovo@gmail.com>
2014-11-07 19:40:09 -0800

    Add submit functionality to review form

--- app.js
+++ app.js
@@ -25,6 +25,10 @@
 
   app.controller('ReviewController', function(){
     this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    }
   });
 
   var gems = [
